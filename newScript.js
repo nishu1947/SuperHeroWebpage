847f5f54261dc6f0a3884c4e98a23fd1534feb3f
Nishant <kashyap.nishant008@gmail.com>
2022-09-01 08:56:13 +0530

    Final Commit

--- newScript.js
+++ newScript.js
@@ -11,7 +11,7 @@ function showSuperHero(data){
     main.innerHTML=' ';
     data.forEach(superhero =>{
         const name= superhero.name;
-        const{strength, speed, power, combat, durability, intelligence}=superhero.powerstats;
+        const{strength, speed, power, combat}=superhero.powerstats;
         const superheroE1= document.createElement('div');
         superheroE1.classList.add('superhero');
         superheroE1.innerHTML=`<img src="${superhero.images.lg}" alt="${name}" class="img">
@@ -28,10 +28,6 @@ function showSuperHero(data){
         <div id="D2">
         <div class="attribute-container"><p class="attributes">Power: <p class="inner-attribute"> ${" " + power}</p></p></div>
         <div class="attribute-container"><p class="attributes">Combat: <p class="inner-attribute"> ${" " + combat}</p></p></div>
-        </div>
-        <div id="D3">
-        <div class="attribute-container"><p class="attributes">Durability: <p class="inner-attribute"> ${" " + durability}</p></p></div>
-        <div class="attribute-container"><p class="attributes">Intelligence: <p class="inner-attribute"> ${" " + intelligence}</p></p></div>
         </div>
         </div>
        `
